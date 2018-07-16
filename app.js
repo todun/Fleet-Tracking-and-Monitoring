947ca6b9257637c4e700cb608ef1e703bab1475e
sateesh-peetha <41141675+sateesh-peetha@users.noreply.github.com>
2018-07-16 14:57:08 +0530

    app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+// Setup basic express server
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 80;
+
+server.listen(port, () => {
+  console.log('Server listening at port %d', port);
+});
+
+// Routing
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Location Tracking
+
+var numUsers = 0;
+
+io.on('connection', (socket) => {
+
+
+});
