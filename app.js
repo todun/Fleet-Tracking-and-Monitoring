6b16a22a6a0546c9ab36d45e8cd2d5719cd73e73
sateesh-peetha <41141675+sateesh-peetha@users.noreply.github.com>
2018-07-16 18:52:20 +0530

    Updated with markers and active drone list

--- app.js
+++ app.js
@@ -15,9 +15,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Location Tracking
 
-var numUsers = 0;
+var dronestate = {};
 
-io.on('connection', (socket) => {
 
+dronestate = {
+
+	  1 : [1,'1.0.0', 12.97 , 77.59]
+	, 2 : [2,'1.0.0', 12.59 , 77.644]
+	, 3 : [3,'1.0.0', 12.78 , 77.78]
+}
+
+counter = 0;
+
+io.on('connection', function(socket) {
+
+   	socket.emit('init' , dronestate);
+
+	socket.on('update',function(data) {
+
+		counter = counter + 1;
+
+		console.log('event rec' , counter);
+
+        //socket.broadcast.emit('message' , dronestate[2]);
+
+	});
 
 });
--- public/tracking.js
+++ public/tracking.js
@@ -1,20 +1,157 @@
-
-
 var map;
-	function initMap() {
-	  map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: -34.397, lng: 150.644},
-		zoom: 10
-	  });
-  }
 
-var socket = io.connect();
+var dronestate = {};
+var dronespeed = [];
 
-socket.on('message', function(data) {
-console.log(data);
-$scope.test = data;
+var markers = [];
 
-});
+function initMap() {
+    var myLatLng = {
+        lat: 12.363,
+        lng: 77.044
+    };
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 5,
+        center: myLatLng
+    });
+}
+
+function addMarker(drone) {
+   // console.log('called');
+    var marker = new google.maps.Marker({
+        position: drone.location,
+        zoom: 5,
+        label: drone.label,
+        map: map
+    });
+    markers.push(marker);
+}
+
+function deleteMarkers() {
+
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+    }
+}
 
 
+function drawMarkers(state) {
 
+    deleteMarkers();
+
+    for (var i in state) {
+
+        drone = {
+            label: i,
+            location: {
+                lat: state[i][2],
+                lng: state[i][3]
+            }
+        };
+
+        addMarker(drone);
+
+    }
+
+    updateActive();
+}
+
+
+function updateActive() {
+
+var activeHTML = ' ';
+
+for ( i in dronespeed ) {
+
+//console.log("test...");
+	activeHTML = activeHTML + ' <br> Drone ' + i + ' - ' +  dronespeed[i] + ' MPH  ';
+
+}
+
+
+document.getElementById('active').innerHTML = activeHTML;
+
+}
+
+    function distance_on_geoid(lat1, lon1, lat2, lon2) {
+
+        // Convert degrees to radians
+        lat1 = lat1 * M_PI / 180.0;
+        lon1 = lon1 * M_PI / 180.0;
+
+        lat2 = lat2 * M_PI / 180.0;
+        lon2 = lon2 * M_PI / 180.0;
+
+        // radius of earth in metres
+        r = 6378100;
+
+
+        rho1 = r * cos(lat1);
+        z1 = r * sin(lat1);
+        x1 = rho1 * cos(lon1);
+        y1 = rho1 * sin(lon1);
+
+
+        rho2 = r * cos(lat2);
+        z2 = r * sin(lat2);
+        x2 = rho2 * cos(lon2);
+        y2 = rho2 * sin(lon2);
+
+
+        dot = (x1 * x2 + y1 * y2 + z1 * z2);
+        cos_theta = dot / (r * r);
+
+        theta = acos(cos_theta);
+
+        // Calculated Distance in Metres
+        return r * theta;
+    }
+
+    var counter = 0;
+    var socket = io.connect();
+
+    //  draw the current state of all drone locations.
+
+    socket.on('init', function(state) {
+
+
+        dronestate = state;
+
+        for (i in dronestate) {
+            dronespeed[i] = 0.0000000000;
+        }
+
+         drawMarkers(state);
+
+
+    });
+
+
+  socket.on('update', function(data) {
+
+        counter = counter + 1;
+
+        if (dronestate[data[0]] === undefined) {
+
+            dronestate[data[0]] = data;
+            drawMarkers(dronestate);
+        } else {
+
+            var p1 = dronestate[data[0]];
+            var p2 = data;
+
+
+            dronestate[data[0]] = data;
+
+            dist = distance_on_geoid(p1[2], p1[3], p2[2], p2[3]);
+
+            time_s = (p2[1] - p1[1]) / 1000.0;
+
+            speed_mps = dist / time_s;
+            dronespeed[data[0]] = speed_mps;
+
+            drawMarkers(dronestate);
+        }
+
+    });
